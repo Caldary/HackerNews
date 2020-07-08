5e8943ca25dea0bbf140adf9cda99da523df925d
Paulo Rocha <paulorocha1988@gmail.com>
2020-07-08 11:52:26 +0100

    fetch is working

---
+++ src/components/NewsList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import News from "./News";
+
+const NewsList = ({news}) => {
+    // const NewsNodes = news.map(news => {
+    //     return (
+    //         <News key={news.id} title={news.title}> 
+    //             {news.url}
+    //         </News>
+    //     );
+    //     });
+
+        return (
+            <h3>
+                hello
+            </h3>
+        )
+    } 
+
+    export default NewsList;
--- src/containers/NewsBox.js
+++ src/containers/NewsBox.js
@@ -1,9 +1,37 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import NewsList from '../components/NewsList';
 
 class NewsBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    }
+  }
+
+  fetchStories() {
+    const url = "https://hacker-news.firebaseio.com/v0/topstories.json";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const promises = data.slice(0, 10).map((storyId) => {
+          return (fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+            .then(res => res.json())
+          )
+        })
+        Promise.all(promises)
+          .then(data => this.setState({ news: data })
+          )
+      })
+  }
+
+  componentDidMount() {
+    this.fetchStories()
+  }
+
   render() {
-    return(
-      <h1> things </h1> 
+    return (
+      <NewsList />
     )
   }
 }
